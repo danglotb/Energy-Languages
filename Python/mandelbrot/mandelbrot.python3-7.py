d0fca45f09569be14d755f1c606b97c9b16365b7
Benjamin Danglot <bdanglot@gmail.com>
2020-10-05 10:43:50 +0200

    feat: change the mandelbrot benchmark by a multi-threaded mandelbrot benchmark for Python

--- Python/mandelbrot/mandelbrot.python3-7.py
+++ Python/mandelbrot/mandelbrot.python3-7.py
@@ -1,74 +1,29 @@
-# The Computer Language Benchmarks Game
-# http://benchmarksgame.alioth.debian.org/
-#
-# contributed by Joerg Baumann
+from numpy           import linspace, reshape
+from matplotlib      import pyplot
+from multiprocessing import Pool
+import sys
 
-from contextlib import closing
-from itertools import islice
-from os import cpu_count
-from sys import argv, stdout
+xmin, xmax = -2.0 ,0.5   # x range
+ymin, ymax = -1.25,1.25  # y range
+nx  , ny   =  int(sys.argv[1]),int(sys.argv[1])  # resolution
+maxiter    =  50         # max iterations
 
-def pixels(y, n, abs):
-    range7 = bytearray(range(7))
-    pixel_bits = bytearray(128 >> pos for pos in range(8))
-    c1 = 2. / float(n)
-    c0 = -1.5 + 1j * y * c1 - 1j
-    x = 0
-    while True:
-        pixel = 0
-        c = x * c1 + c0
-        for pixel_bit in pixel_bits:
-            z = c
-            for _ in range7:
-                for _ in range7:
-                    z = z * z + c
-                if abs(z) >= 2.: break
-            else:
-                pixel += pixel_bit
-            c += c1
-        yield pixel
-        x += 8
+def mandelbrot(z): # computation for one pixel
+  c = z
+  for n in range(maxiter):
+    if abs(z)>2: return n  # divergence test
+    z = z*z + c
+  return maxiter
 
-def compute_row(p):
-    y, n = p
+X = linspace(xmin,xmax,nx) # lists of x and y
+Y = linspace(ymin,ymax,ny) # pixel co-ordinates
 
-    result = bytearray(islice(pixels(y, n, abs), (n + 7) // 8))
-    result[-1] &= 0xff << (8 - n % 8)
-    return y, result
+# main loops
+p = Pool()
+Z = [complex(x,y) for y in Y for x in X]
+N = p.map(mandelbrot,Z)
 
-def ordered_rows(rows, n):
-    order = [None] * n
-    i = 0
-    j = n
-    while i < len(order):
-        if j > 0:
-            row = next(rows)
-            order[row[0]] = row
-            j -= 1
+N = reshape(N, (nx,ny)) # change to rectangular array
 
-        if order[i]:
-            yield order[i]
-            order[i] = None
-            i += 1
-
-def compute_rows(n, f):
-    row_jobs = ((y, n) for y in range(n))
-
-    if cpu_count() < 2:
-        yield from map(f, row_jobs)
-    else:
-        from multiprocessing import Pool
-        with Pool() as pool:
-            unordered_rows = pool.imap_unordered(f, row_jobs)
-            yield from ordered_rows(unordered_rows, n)
-
-def mandelbrot(n):
-    write = stdout.buffer.write
-
-    with closing(compute_rows(n, compute_row)) as rows:
-        write("P4\n{0} {0}\n".format(n).encode())
-        for row in rows:
-            write(row[1])
-
-if __name__ == '__main__':
-    mandelbrot(int(argv[1]))
+pyplot.imshow(N) # plot the image
+pyplot.show()
